368b634ae68fb89af5d5dca64e41bc8ce17cca06
Sławomir Malinowski <slawomir.malinowski@o2.pl>
2019-04-01 17:21:30 +0200

    Update tasks.h

--- PROGRAM/tasks.h
+++ PROGRAM/tasks.h
@@ -7,17 +7,17 @@
 #define	true	!(false)
 
 
-extern uint8_t pb4sem; //naciœniêcie przycisku "4" powoduje ustawienie semafora
-extern uint16_t system_tick, tick_500ms; //liczniki inkrementowane przerwaniami z przepe³nienia timera2			
-extern uint8_t start_TX_RX_seq_sem; //znacznik rozpoczêcia transmisji przez modu³ RFM69H
-extern uint8_t end_TX_RX_seq_sem; //znacznik zakoñczenia transmisji przez modu³ RFM69H
+extern uint8_t pb4sem; //naciÅ›niÄ™cie przycisku "4" powoduje ustawienie semafora
+extern uint16_t system_tick, tick_500ms; //liczniki inkrementowane przerwaniami z przepeÅ‚nienia timera2			
+extern uint8_t start_TX_RX_seq_sem; //znacznik rozpoczÄ™cia transmisji przez moduÅ‚ RFM69H
+extern uint8_t end_TX_RX_seq_sem; //znacznik zakoÅ„czenia transmisji przez moduÅ‚ RFM69H
 
 /*prototypy funkcji pomocniczych */
 //void init_crc8(void);
 void RFM69H_SPI_write(uint16_t address_data);
 uint8_t RFM69H_SPI_read(uint8_t address);
 
-/* prototypy funkcji tasków */
+/* prototypy funkcji taskÃ³w */
 void greenLEDblink_func(uint16_t LED_on_off);
 uint16_t send_sequence_func(uint8_t *seq_sent_successPtr);
 
